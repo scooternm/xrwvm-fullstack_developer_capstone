3831567c34af5fddf27157906cf03e35e524cb38
Kayla McCray <133404243+scooternm@users.noreply.github.com>
2024-03-21 19:09:23 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -23,9 +23,7 @@ class CarModel(models.Model):
     year = models.IntegerField(default=2023,
         validators=[
             MaxValueValidator(2023),
-            MinValueValidator(2015)
-
-            ])
+            MinValueValidator(2015)])
 
     def __str__(self):
         return self.name
