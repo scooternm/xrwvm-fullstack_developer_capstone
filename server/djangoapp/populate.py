bcf8b1043633a729b2043bebd0e4c5da50fa0642
Kayla McCray <133404243+scooternm@users.noreply.github.com>
2024-03-21 18:26:45 -0400

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -2,11 +2,11 @@ from .models import CarMake, CarModel
 
 def initiate():
     car_make_data = [
-        {"name":"NISSAN", "description":"Great cars. Japanese technology"},
-        {"name":"Mercedes", "description":"Great cars. German technology"},
-        {"name":"Audi", "description":"Great cars. German technology"},
-        {"name":"Kia", "description":"Great cars. Korean technology"},
-        {"name":"Toyota", "description":"Great cars. Japanese technology"},
+        {"name": "NISSAN", "description":"Great cars. Japanese technology"},
+        {"name": "Mercedes", "description":"Great cars. German technology"},
+        {"name": "Audi", "description":"Great cars. German technology"},
+        {"name": "Kia", "description":"Great cars. Korean technology"},
+        {"name": "Toyota", "description":"Great cars. Japanese technology"},
     ]
 
     car_make_instances = []
@@ -15,8 +15,8 @@ def initiate():
 
 
     car_model_data = [
-      {"name":"Pathfinder", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
-      {"name":"Qashqai", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
+      {"name": "Pathfinder", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
+      {"name": "Qashqai", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
       {"name":"XTRAIL", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
       {"name":"A-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
       {"name":"C-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
