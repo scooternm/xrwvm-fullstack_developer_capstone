bab74e36c30b3024c5bb159d17fd05de7936a223
Kayla McCray <133404243+scooternm@users.noreply.github.com>
2024-03-22 10:53:01 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -95,8 +95,8 @@ def get_dealer_reviews(request,dealer_id):
 
 def get_dealer_details(request, dealer_id):
     if(dealer_id):
-    endpoint = "/fetchDealer/"+str(dealer_id)
-    dealership = get_request(endpoint)
+        endpoint = "/fetchDealer/"+str(dealer_id)
+        dealership = get_request(endpoint)
     return JsonResponse({"status": 200,"dealer": dealership})
 else:
     return JsonResponse({"status": 400,"message": "Bad Request"})
