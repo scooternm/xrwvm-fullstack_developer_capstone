4eddc76047f60719649ff4b9f2f319f5f13bce0e
Kayla McCray <133404243+scooternm@users.noreply.github.com>
2024-03-22 14:07:01 -0400

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -1,11 +1,12 @@
+/*jshint esversion: 8 */
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -33,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -72,7 +73,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
         const documents = await Dealerships.find({state: req.params.state});
         res.json(documents);
     }   catch(error) {
-        res.status(500).json({ error: 'Error fetching dealer'})
+        res.status(500).json({ error: 'Error fetching dealer'});
     }
 
 
@@ -82,17 +83,17 @@ app.get('/fetchDealers/:state', async (req, res) => {
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
     try {
-        const documents = await Dealerships.find({id: req.params.id})
+        const documents = await Dealerships.find({id: req.params.id});
         res.json(documents);
     }   catch(error) {
-        res.status(500).json({error: 'Error fetching dealer'})
+        res.status(500).json({error: 'Error fetching dealer'});
     }
 });
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
+  const documents = await Reviews.find().sort( { id: -1 } );
   let new_id = documents[0]['id']+1
 
   const review = new Reviews({
