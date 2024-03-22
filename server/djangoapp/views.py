94a77e99e276a3feaba61d6ab0985c26b1a0f830
Kayla McCray <133404243+scooternm@users.noreply.github.com>
2024-03-22 11:55:02 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -44,8 +44,10 @@ def registration(request):
     except Exception as e:
         logger.debug(f"{username} is new user:{str(e)}")
     if not username_exist:
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=
-                                        last_name, password=password, email=email)
+        user = User.objects.create_user(username=username, 
+                                        first_name=first_name,
+                                        last_name=last_name, 
+                                        password=password, email=email)
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
