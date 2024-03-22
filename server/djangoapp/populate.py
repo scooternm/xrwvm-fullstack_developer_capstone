5d9737980792087f7f5aa360fcae392b41fb1744
Kayla McCray <133404243+scooternm@users.noreply.github.com>
2024-03-22 09:29:28 -0400

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,26 +12,27 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+        car_make_instances.append(CarMake.objects.create(name=data['name'],
+                                                         description=data['description']))
 
     car_model_data = [
-      {"name": "Pathfinder", "type": "SUV", 
+      {"name": "Pathfinder", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[0]},
       {"name": "Qashqai", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[0]},
-      {"name": "XTRAIL", "type": "SUV", 
+      {"name": "XTRAIL", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[0]},
       {"name": "A-Class", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[1]},
-      {"name": "C-Class", "type": "SUV", 
+      {"name": "C-Class", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[1]},
       {"name": "E-Class", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[1]},
-      {"name": "A4", "type": "SUV", 
+      {"name": "A4", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[2]},
       {"name": "A5", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[2]},
-      {"name": "A6", "type": "SUV", 
+      {"name": "A6", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[2]},
       {"name": "Sorrento", "type": "SUV",
        "year": 2023, "car_make": car_make_instances[3]},
@@ -48,4 +49,5 @@ def initiate():
     ]
 
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'],
+                                type=data['type'], year=data['year'])
